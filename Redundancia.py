fbe6ba9a6e51e921923b8640e5636c6a4008bead
ZabDS <zabdiel1297@gmail.com>
2021-05-06 18:39:27 -0500

    Creacion de IG de usuario

--- Redundancia.py
+++ Redundancia.py
@@ -2,11 +2,16 @@
 import Pyro4
 import Pyro4.naming as naming
 import threading
-import DBConnector
+import DBConnectorRed as DBConnector
 import random
 import datetime
 from  tkinter import Tk,Canvas,PhotoImage,Button
 from PIL import ImageTk, Image
+
+#La cola es controlada por el servidor de redundancia
+global bookQueue
+bookQueue = list(range(9))
+random.shuffle(bookQueue)
 
 def initDNS(port):
     #Thread for start the DNS
@@ -18,7 +23,7 @@ class Interfaz(threading.Thread):
     def run(self):    
         self.root = Tk()
         self.root.geometry("1080x480")
-        self.root.title("Coordinador")
+        self.root.title("Redundancia")
         self.canvas = Canvas(self.root,width = "395",height = "550", background="black")
         self.canvas.grid(column=0, row=0)
         self.ImgCanvas = PhotoImage(file="")
@@ -28,57 +33,80 @@ class Interfaz(threading.Thread):
         self.root.mainloop()
     
     def setPortada(self,routeImg):
-        print(routeImg)
+        #print(routeImg)
         self.ImgCanvas = ImageTk.PhotoImage(Image.open(routeImg))
         self.canvas.create_image(50, 10, image=self.ImgCanvas, anchor="nw")
 
     def buttonActionRestart(self):
-        uriPrincipalCoordinator = "PYRONAME:principal@127.0.0.1:9091"
-        DBServerMS = Pyro4.Proxy(uriPrincipalCoordinator) 
-        self.ImgCanvas = PhotoImage(file="")
+        uriRespaldoCoordinator = "PYRONAME:Redundancia@127.0.0.1:9092"
+        DBServerMS = Pyro4.Proxy(uriRespaldoCoordinator)
+        self.ImgCanvas = PhotoImage(file="") 
         DBServerMS.resetSys()
 
 
 window = Interfaz()
 
+#Objeto que será compartido a través de la red
 @Pyro4.expose
 class DBServer():
     def __init__(self):
-        self.DBM = DBConnector.DBManager("zabdiel","idscom","127.0.0.1","LibraryDB")
-        self.uriRedundacia = "PYRONAME:Redundancia@127.0.0.1:9092"
-        self.DBServerR = Pyro4.Proxy(self.uriRedundacia)
+        #Conector de BD
+        self.DBM = DBConnector.DBManager("zabdiel","idscom","127.0.0.1","LibraryDBR")
 
+    #Método para obtener un libro
     def getBook(self,ID_Usuario,horaI):
-        numLibros = len(self.DBServerR.getBQueue())
+        global bookQueue
+        numLibros = len(bookQueue)
         if numLibros > 0:
-            idLibro = self.DBServerR.popBQueue()+1
+            idLibro = bookQueue.pop()+1
             libro = self.DBM.getLibroByID(idLibro)
             
             routeImg = libro[5]
-            #print(routeImg)
             window.setPortada(routeImg)
+
             fecha = datetime.datetime.today()
             now = datetime.datetime.now()
             horaF=datetime.time(now.hour,now.minute,now.second,now.microsecond)
 
+            #Escribe los calculos de tiempo y los id's a la bd
             (NumPedido,NumSesion) = self.DBM.setPedido(ID_Usuario,idLibro,fecha,horaI,horaF)
-            self.DBServerR.BUPedido(NumPedido,NumSesion,ID_Usuario,idLibro,fecha,horaI,horaF,routeImg)
             return (NumPedido,libro)
         else:
             return -1
-    def anounceMe(self,IP,Nombre):
-        ID = self.DBM.setUsuario(IP,Nombre)
-        self.DBServerR.anounceMe(IP,Nombre,int(ID))
-        return ID
+    #Método para el BackUp del pedido hecho en la bd del BackUp
+    def BUPedido(self,ID_Pedido,ID_Sesion,ID_Usuario,idLibro,fecha,horaI,horaF,routeImg):
+        self.DBM.setPedidoBU(ID_Pedido,ID_Sesion,ID_Usuario,idLibro,fecha,horaI,horaF)
+        window.setPortada(routeImg)
 
+    #Método que sirve para anunciar que un nuevo cliente se ha conectado y se registre a la BD
+    def anounceMe(self,IP,Nombre,ID_usr=-1):
+        if(ID_usr < 0):
+            ID = self.DBM.setUsuario(IP,Nombre)
+            return ID
+        else:
+            ID_usr = self.DBM.setUsuario(IP,Nombre,ID_usr)
+            return ID_usr
+    #Método para reiniciar el sistema y limpiar la base de datos
     def resetSys(self):
-        self.DBServerR.restartBQueue()
+        global bookQueue
+        bookQueue = self.restartBQueue()
         self.DBM.resetDB()
-        self.DBServerR.resetSys()
-        print("Sistema reiniciado")
+    #Método de prueba de conexión; útil para los usuarios
+    def connTest(self):
+        return 1
+    #Métodos para el control de la cola de libros. Utilies para su manejo en el coordinador principal
+    def getBQueue(self):
+        return bookQueue
+    def popBQueue(self):
+        return bookQueue.pop()
+    def restartBQueue(self):
+        NewbookQueue = list(range(9))
+        random.shuffle(NewbookQueue)
+        bookQueue=NewbookQueue
+        return bookQueue
 
-
-port = 9091
+#MAIN
+port = 9092
 daemon = Pyro4.Daemon()                # make a Pyro daemon
 
 DNServer = threading.Thread(target=initDNS,args=(port,) ,name='DNS')
@@ -86,7 +114,7 @@ DNServer.start()
     
 ns = Pyro4.locateNS(port=port)                  # find the name server
 uri = daemon.register(DBServer)   # register the greeting maker as a Pyro object
-ns.register("principal", uri)   # register the object with a name in the name server
+ns.register("Redundancia", uri)   # register the object with a name in the name server
 
 print("Ready.")
 window.start()
--- Usuario.py
+++ Usuario.py
@@ -2,6 +2,8 @@ import Pyro4
 import time
 import socket
 import datetime
+from  tkinter import Tk,Button,Label,messagebox
+import threading
 
 
 uriPrincipalCoordinator = "PYRONAME:principal@127.0.0.1:9091"
@@ -9,7 +11,9 @@ uriRespaldoCoordinator = "PYRONAME:Redundancia@127.0.0.1:9092"
 DBServer = Pyro4.Proxy(uriPrincipalCoordinator) 
 DBServerR = Pyro4.Proxy(uriRespaldoCoordinator)
 
-
+root=Tk()
+root.geometry("720x480")
+root.title("Usuario")
 
 class Usuario():
     def __init__(self):
@@ -17,7 +21,7 @@ class Usuario():
         self.Server = DBServer
         self.IP= socket.gethostbyname(self.Name)
         self.ID = DBServer.anounceMe(self.IP,self.Name)
-
+    
     def changeServer(self):
         print("Intentando conectarse con el coordinador de respaldo...")
         self.Server = DBServerR
@@ -27,23 +31,80 @@ class Usuario():
         try:
             now = datetime.datetime.now()
             horaI=datetime.time(now.hour,now.minute,now.second,now.microsecond)
-            print(self.Server.getBook(self.ID,horaI))
-            time.sleep(1)
+            Libro = self.Server.getBook(self.ID,horaI) 
+            #print(self.Server.getBook(self.ID,horaI))
+            return (1,Libro)
         except Pyro4.errors.ConnectionClosedError as e:
             print("Error al conectarse con el coordinador principal")
             print(e)
             self.changeServer()
-    
-NewUsuario = Usuario()
+            return (-1,"")
+    def reset(self):
+        self.Server.resetSys()
 
-i=0
-while (i<5):
-    i = i+1
-    #DBServer.resetSys()
-    try:
-        NewUsuario.getBook()
-    except Pyro4.errors.ConnectionClosedError as e:
-        print("Error al conectarse con el coordinador principal")
-        print(e)
-        NewUsuario.changeServer()
-    #time.sleep(1)
+class Interfaz(threading.Thread):
+    def __init__(self,time):
+        threading.Thread.__init__(self)
+        self.timeForLook = time
+        self.label = Label(root,font=("times",50,"bold"))
+        self.labelBook = Label(root,font=("times",25,"bold"))
+        self.buttonReset = Button(root, text="Reiniciar")
+        self.buttonBook = Button(root, text="Pedir Libro")
+        self.Usuario = Usuario()
+
+    def run(self):
+        self.label.grid(row=0,column=0,pady=25,padx=100)
+        self.buttonReset.grid(row=1,column=0,pady=25,padx=100)
+        self.buttonReset.config(command = self.buttonActionRestart)
+        self.buttonBook.grid(row=2,column=0,pady=25,padx=100)
+        self.buttonBook.config(command = self.buttonActionBook)
+        self.labelBook.grid(row=3,column=0,pady=25,padx=100)
+        self.time()
+
+    def time(self):
+        while True:    
+            textLabel = str(self.timeForLook[2])+":"+str(self.timeForLook[1])+":"+str(self.timeForLook[0])
+            self.label.config(text=textLabel)
+            self.timeForLook[0]=self.timeForLook[0]+1
+            time.sleep(1)
+            if (self.timeForLook[0]>59):
+                self.timeForLook[0]=0
+                self.timeForLook[1]=self.timeForLook[1]+1
+            if (self.timeForLook[1]>59):
+                self.timeForLook[1]=0
+                self.timeForLook[2]=self.timeForLook[2]+1
+            if (self.timeForLook[2]>23):
+                self.timeForLook[2]=0
+#Aqui no se si deberamos eliminar el if
+    def buttonActionBook(self):
+        (cod,Libro) = self.Usuario.getBook()
+        print(Libro)
+        if cod < 0:
+            self.labelBook.config(text = "Erro, intente de nuevo")
+        elif type(Libro) != int:
+            self.labelBook.config(text = Libro[1][1])
+        else:
+            answer = messagebox.askyesnocancel("Ya no quedan libros", "Desea salir de la tienda?")
+            if answer == True:
+                exit(1)
+
+    def buttonActionRestart(self):
+        self.Usuario.reset()
+
+now=datetime.datetime.now()
+NewUsuario = Interfaz([now.second,now.minute,now.hour])
+NewUsuario.start()
+root.mainloop()
+
+
+#i=0
+#while (i<5):
+#    i = i+1
+#    #DBServer.resetSys()
+#    try:
+#        NewUsuario.getBook()
+#    except Pyro4.errors.ConnectionClosedError as e:
+#        print("Error al conectarse con el coordinador principal")
+#        print(e)
+#        NewUsuario.changeServer()
+#    #time.sleep(1)
